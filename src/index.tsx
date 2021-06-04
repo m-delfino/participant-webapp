6637c00786e103b67f58b32853c0b565aadd3cf5
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-04 16:44:50 +0200

    add default theme call via index.scss

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,15 @@ import {
 
 import { LoadingPlaceholder } from 'case-web-ui';
 
+import './index.scss';
+
 import '@fontsource/open-sans';
 // import '@fontsource/open-sans/300.css'; // for light font
 import '@fontsource/open-sans/400-italic.css';
 import '@fontsource/open-sans/700.css';
-
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 import '@fortawesome/fontawesome-free/js/fontawesome';
-
-import 'case-web-ui/build/scss/theme-default.scss';
 
 import 'bootstrap/dist/js/bootstrap.bundle';
 
